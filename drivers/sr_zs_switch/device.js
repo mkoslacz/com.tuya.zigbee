2cf1e8dc2483f8e1da6cb0a2ebdfad1c10ee6d85
Mateusz Koślacz <mateusz.koslacz@gmail.com>
2024-12-04 17:27:11 +0100

    Working switch 3 at once

--- drivers/sr_zs_switch/device.js
+++ drivers/sr_zs_switch/device.js
@@ -8,54 +8,71 @@ class SRZSSwitch extends ZigBeeDevice {
     async onNodeInit({zclNode}) {
         this.printNode();
 
-        // Rejestracja listenerów
-        this.registerCapabilityListener('onoff_1', async (value) => {
-            return this.sendOnOffCommand(1, value);
-        });
-        
-        this.registerCapabilityListener('onoff_2', async (value) => {
-            return this.sendOnOffCommand(2, value);
-        });
-        
-        this.registerCapabilityListener('onoff_3', async (value) => {
-            return this.sendOnOffCommand(3, value);
-        });
-
-        // Nasłuchiwanie raportów
-        for (let endpoint = 1; endpoint <= 3; endpoint++) {
-            zclNode.endpoints[endpoint].clusters[CLUSTER.ON_OFF.NAME]
-                .on('attr.onOff', value => {
-                    this.handleReport(`onoff_${endpoint}`, value);
-                });
-        }
-    }
-
-    async sendOnOffCommand(endpoint, value) {
-        try {
-            const cluster = this.zclNode.endpoints[endpoint].clusters[CLUSTER.ON_OFF.NAME];
-            
-            if (value) {
-                await cluster.on();
-            } else {
-                await cluster.off();
+        // Przełącznik 1
+        await this.configureAttributeReporting([
+            {
+                endpointId: 1,
+                cluster: CLUSTER.ON_OFF,
+                attributeName: 'onOff',
+                minInterval: 0,
+                maxInterval: 300,
+                minChange: 1
             }
-            
-            return value;
-        } catch (error) {
-            this.error(`Error sending command to endpoint ${endpoint}:`, error);
-            throw error;
-        }
-    }
+        ]);
 
-    async handleReport(capability, value) {
-        try {
-            const currentValue = await this.getCapabilityValue(capability);
-            if (currentValue !== value) {
-                await this.setCapabilityValue(capability, value);
+        this.registerCapability('onoff_1', CLUSTER.ON_OFF, {
+            endpoint: 1,
+            get: 'onOff',
+            getOpts: {},
+            set: 'toggle',
+            setParser: () => ({}),
+            report: 'onOff',
+            reportParser: value => value === 1
+        });
+
+        // Przełącznik 2
+        await this.configureAttributeReporting([
+            {
+                endpointId: 2,
+                cluster: CLUSTER.ON_OFF,
+                attributeName: 'onOff',
+                minInterval: 0,
+                maxInterval: 300,
+                minChange: 1
             }
-        } catch (error) {
-            this.error('Error handling report:', error);
-        }
+        ]);
+
+        this.registerCapability('onoff_2', CLUSTER.ON_OFF, {
+            endpoint: 2,
+            get: 'onOff',
+            getOpts: {},
+            set: 'toggle',
+            setParser: () => ({}),
+            report: 'onOff',
+            reportParser: value => value === 1
+        });
+
+        // Przełącznik 3
+        await this.configureAttributeReporting([
+            {
+                endpointId: 3,
+                cluster: CLUSTER.ON_OFF,
+                attributeName: 'onOff',
+                minInterval: 0,
+                maxInterval: 300,
+                minChange: 1
+            }
+        ]);
+
+        this.registerCapability('onoff_3', CLUSTER.ON_OFF, {
+            endpoint: 3,
+            get: 'onOff',
+            getOpts: {},
+            set: 'toggle',
+            setParser: () => ({}),
+            report: 'onOff',
+            reportParser: value => value === 1
+        });
     }
 }
 
