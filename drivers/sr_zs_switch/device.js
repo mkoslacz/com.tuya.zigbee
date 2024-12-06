4bdff6831f83a3e972b7770bb7ebedb75556f9bd
Mateusz Koślacz <mateusz.koslacz@gmail.com>
2024-12-06 21:01:33 +0100

    refactor

--- drivers/sr_zs_switch/device.js
+++ drivers/sr_zs_switch/device.js
@@ -8,86 +8,45 @@ class SRZSSwitch extends ZigBeeDevice {
     async onNodeInit({ zclNode }) {
         this.printNode();
 
-        // Przełącznik 1
+      for (let i = 1; i <= 3; i++) {
         await this.configureAttributeReporting([
-            {
-                endpointId: 1,
-                cluster: CLUSTER.ON_OFF,
-                attributeName: 'onOff',
-                minInterval: 0,
-                maxInterval: 300,
-                minChange: 1
-            }
+          {
+            endpointId: i,
+            cluster: CLUSTER.ON_OFF,
+            attributeName: 'onOff',
+            minInterval: 0,
+            maxInterval: 300,
+            minChange: 1
+          }
         ]);
 
-        this.registerCapability('onoff_1', CLUSTER.ON_OFF, {
-            endpoint: 1,
-            get: 'onOff',
-            getOpts: {},
-            set: 'toggle',
-            setParser: () => ({}),
-            report: 'onOff',
-            reportParser:  value => value
+        this.registerCapability(`onoff_${i}`, CLUSTER.ON_OFF, {
+          endpoint: i,
+          get: 'onOff',
+          getOpts: {},
+          set: 'toggle',
+          setParser: () => ({}),
+          report: 'onOff',
+          reportParser: value => value
         });
+      }
 
-        // Przełącznik 2
-        await this.configureAttributeReporting([
-            {
-                endpointId: 2,
-                cluster: CLUSTER.ON_OFF,
-                attributeName: 'onOff',
-                minInterval: 0,
-                maxInterval: 300,
-                minChange: 1
-            }
-        ]);
-
-        this.registerCapability('onoff_2', CLUSTER.ON_OFF, {
-            endpoint: 2,
-            get: 'onOff',
-            getOpts: {},
-            set: 'toggle',
-            setParser: () => ({}),
-            report: 'onOff',
-            reportParser:  value => value
-        });
-
-        // Przełącznik 3
-        await this.configureAttributeReporting([
-            {
-                endpointId: 3,
-                cluster: CLUSTER.ON_OFF,
-                attributeName: 'onOff',
-                minInterval: 0,
-                maxInterval: 300,
-                minChange: 1
-            }
-        ]);
-
-        this.registerCapability('onoff_3', CLUSTER.ON_OFF, {
-            endpoint: 3,
-            get: 'onOff',
-            getOpts: {},
-            set: 'toggle',
-            setParser: () => ({}),
-            report: 'onOff',
-            reportParser: value => value
-        });
-
-        // await device.getEndpoint(1).read('genBasic', ['manufacturerName', 'zclVersion', 'appVersion', 'modelId', 'powerSource', 0xfffe]);
-
-        await zclNode.endpoints[1].clusters.basic.readAttributes([
-            'manufacturerName',
-            'zclVersion',
-            'appVersion',
-            'modelId',
-            'powerSource',
-            'attributeReportingStatus'
-        ])
-            .catch(err => {
-                this.error('Error when reading device attributes ', err);
-            });
+      await this.magicallyConfigureTuyaSeparateOnoffSwitchingOnEndpoints(zclNode);
     }
+
+  async magicallyConfigureTuyaSeparateOnoffSwitchingOnEndpoints(zclNode) {
+    await zclNode.endpoints[1].clusters.basic.readAttributes([
+      'manufacturerName',
+      'zclVersion',
+      'appVersion',
+      'modelId',
+      'powerSource',
+      'attributeReportingStatus'
+    ])
+      .catch(err => {
+        this.error('Error when reading device attributes ', err);
+      });
+  }
 }
 
-module.exports = SRZSSwitch; 
+module.exports = SRZSSwitch;
